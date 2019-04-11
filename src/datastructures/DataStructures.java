394550c380ed637293f95c4645802f1458dc0605
Kentec <Kentec@2130-INS-343595.mykendall.mymdc.net>
2019-04-11 11:56:19 -0400

    our data structures commit.

---
+++ src/datastructures/DataStructures.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructures;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kentec
+ */
+public class DataStructures {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList al = new ArrayList();
+        al.add(1);
+        al.add("1");
+        System.out.println(al.get(0));
+    }
+    
+}
